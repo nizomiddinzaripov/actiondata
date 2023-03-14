0564d0b25eb761c9b0b3eaed9e7707889db13860
programm011 <programm011@gmail.com>
2023-03-14 19:25:55 +0500

    upgraded

--- src/ActionDataBase.php
+++ src/ActionDataBase.php
@@ -315,10 +315,15 @@ abstract class ActionDataBase implements ActionDataContract
         $placeholder = new stdClass;
 
         foreach (is_array($keys) ? $keys : func_get_args() as $key) {
-            $value = data_get($input, $key, $placeholder);
+            $value           = data_get($input, $key, $placeholder);
+            $attributesValue = data_get($this->attributes, $key, $placeholder);
 
             if ($value !== $placeholder) {
                 Arr::set($results, $key, $value);
+            }
+
+            if ($attributesValue !== $placeholder) {
+                Arr::set($results, $key, $attributesValue);
             }
         }
 
